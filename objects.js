8e4f7479adc0c44b5fdeeefdc2447e4d6d41831c
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-04 01:05:02 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,5 @@
 //creates an object called "playlist" with one Artist (key) and song (value) 
 var playlist = {
-  "RickAstley" : "Never Gonna Give You Up",
-  "A-Ha" : "Take On Me"
+  "Rick Astley" : "Never Gonna Give You Up"
 }
 
